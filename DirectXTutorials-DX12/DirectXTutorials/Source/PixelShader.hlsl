51d0f4a9f0e27c82d77c0683479cad96d31c8190
R3dPraiseTheSun <70650460+R3dPraiseTheSun@users.noreply.github.com>
2023-11-21 19:58:32 +0200

    added lightsource

--- DirectXTutorials-DX12/DirectXTutorials/Source/PixelShader.hlsl
+++ DirectXTutorials-DX12/DirectXTutorials/Source/PixelShader.hlsl
@@ -1,9 +1,22 @@
 struct PixelShaderInput
 {
-	float4 Color    : COLOR;
+    float4 Color    : COLOR;
+    float3 Normal   : NORMAL;
+    float3 LightDir : TEXCOORD0;
 };
 
 float4 main( PixelShaderInput IN ) : SV_Target
 {
-    return IN.Color;
+     // Calculate diffuse component
+    float diffuseIntensity = max(0.0f, dot(IN.Normal, -IN.LightDir));
+    float3 diffuseColor = float3(1.0f, 1.0f, 1.0f); // White diffuse color
+    float3 diffuse = diffuseIntensity * diffuseColor;
+    
+    // Combine ambient, diffuse, and specular components
+    float3 ambient = float3(0.2f, 0.2f, 0.2f); // Example ambient color
+    float3 finalColor = ambient + diffuse;
+    
+    float alpha = 1.0f;
+    
+    return float4(finalColor, alpha);
 }
--- DirectXTutorials-DX12/DirectXTutorials/Source/VertexShader.hlsl
+++ DirectXTutorials-DX12/DirectXTutorials/Source/VertexShader.hlsl
@@ -13,19 +13,36 @@ struct VertexPosColor
 {
     float3 Position : POSITION;
     float3 Color    : COLOR;
+    float3 Normal   : NORMAL;
+    float3 LightDir : TEXCOORD0;
 };
 
 struct VertexShaderOutput
 {
 	float4 Color    : COLOR;
+    float3 Normal   : NORMAL;
+    float3 LightDir : TEXCOORD0;
     float4 Position : SV_Position;
 };
 
 VertexShaderOutput main(VertexPosColor IN)
 {
     VertexShaderOutput OUT;
-
+    
+    // Calculate the normal matrix to transform normals in world space
+    float3x3 normalMatrix = (float3x3) gModelViewProjection.MVP;
+    normalMatrix[0] = normalize(normalMatrix[0]);
+    normalMatrix[1] = normalize(normalMatrix[1]);
+    normalMatrix[2] = normalize(normalMatrix[2]);
+    
     OUT.Position = mul(gModelViewProjection.MVP, float4(IN.Position, 1.0f));
+    
+    // Transform normal to world space
+    OUT.Normal = normalize(mul(IN.Position, normalMatrix));
+    
+    // Calculate light direction in world space
+    OUT.LightDir = normalize(float3(1.0f, 1.0f, -1.0f)); // Example light direction
+    
     OUT.Color = float4(IN.Color, 1.0f);
 
     return OUT;
